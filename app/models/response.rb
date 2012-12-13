0b189dbb791a74f9a3894a02d6799ed07eb734e3
Abhishek Reddy <abhishekreddy1206@gmail.com>
2012-12-13 14:46:57 -0700

    removed list of responses

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,5 +1,5 @@
 class Response < ActiveRecord::Base
-  attr_accessible :answer, :question_id, :user_id
+  attr_accessible :answer, :question_id, :user_id, :short_answer
   validates :answer, :question_id, :user_id, presence: true
     
   belongs_to :user
